12a7ec4bc5915664a6d429a5775bba655cda465e
Bill Pullen <bill.pull@gmail.com>
2018-12-18 21:58:24 -0500

    Blueprint: Create necessary files

---
+++ blueprints/ember-cli-storybook/files/.storybook/config.js
@@ -0,0 +1,8 @@
+import { configure } from '@storybook/ember';
+
+function loadStories() {
+  // require('../stories/index.js');
+  // You can require as many stories as you need.
+}
+
+configure(loadStories, module);
