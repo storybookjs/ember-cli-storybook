5ba01d9de8c5293e1d555258cb23a244b007b03b
Jonathan Haines <jonno.haines@gmail.com>
2020-11-19 10:21:22 +1100

    add renderStory test helper

---
+++ addon-test-support/render-story.js
@@ -0,0 +1,9 @@
+import { getContext, render } from '@ember/test-helpers'
+
+export default async function renderStory(story, args) {
+  let testContext = getContext();
+  const { context, template } = story(Object.assign({}, story.args, args))
+  Object.assign(testContext, context)
+  await render(template)
+  return context
+}
