73f10ed5cb33ed1ae2b963a26cc2bf3877c56d86
Jonathan Haines <jonno.haines@gmail.com>
2020-11-24 07:33:54 +1100

    Update tests/integration/test-support-test.js
    Co-authored-by: Thomas Gossmann <gossi@users.noreply.github.com>

--- tests/integration/test-support-test.js
+++ tests/integration/test-support-test.js
@@ -1,7 +1,7 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { renderStory } from '@storybook/ember-cli-storybook/test-support';
-import hbs from 'htmlbars-inline-precompile';
+import { hbs } from 'ember-cli-htmlbars';
 
 function storyFn(args) {
   return {
