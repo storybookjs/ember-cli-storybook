d22366e285bcad5d8cf9f4ba7b2dc45031e9f434
Benedikt Deicke <benedikt@benediktdeicke.com>
2022-05-18 18:05:20 +0200

    Updates development dependencies to make tests pass again

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -2,6 +2,11 @@ import Application from 'dummy/app';
 import config from 'dummy/config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
+import { setup } from 'qunit-dom';
+
+import * as QUnit from 'qunit';
+
+setup(QUnit.assert);
 
 setApplication(Application.create(config.APP));
 
